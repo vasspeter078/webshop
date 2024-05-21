451d03be3f962ecb2beb3fcdfa815f3096b8772a
vasspeter078 <109521889+vasspeter078@users.noreply.github.com>
2024-05-21 18:48:28 +0200

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,16 +4,65 @@ import { searchByText } from '../data.js';
 import Product from './Product.js';
 import { useState, useEffect } from 'react';
 
+import Iphone15 from '../iphone15.png';
+import SamsungS23 from '../samsungs23.png';
+import XiaomiRedmiNote12 from '../xiaomiredminote12.png';
+import SamsungGalaxyA54 from '../samsunggalaxya54.png';
+import MacBookAir13 from '../macbookair13.png';
+import ASUSROGStrix17 from '../asusrogstrixg17.png';
+import DellInspiron3511 from '../dellinspiron3511.png';
+import AirPods2 from '../airpods2.png';
+import SonyWH1000XM4 from '../sonywh-1000xm4.png';
+
 const getProducts = async() => {
     const response = await fetch(`http://localhost:5000/api/product`);
     const data = await response.json();
-    return data.products;
+    let dataSet = mapImages(data.products);
+    return dataSet;
 }
 
 const getFilteredProducts = async(searchTerm) => {
     const response = await fetch(`http://localhost:5000/api/product/filter-by-name/${searchTerm}`);
     const data = await response.json();
-    return data.products;
+    let dataSet = mapImages(data.products);
+    return dataSet;
+}
+
+function mapImages(dataSet) {
+    console.log(dataSet.length);
+    for (let i = 0; i < dataSet.length; ++i) {
+        console.log(dataSet[i].image);
+        switch(dataSet[i].image) {
+            case "iphone15":
+                dataSet[i].image = Iphone15;
+                break;  
+            case "samsungs23":
+                dataSet[i].image = SamsungS23;
+                break;  
+            case "xiaomiredminote12":
+                dataSet[i].image = XiaomiRedmiNote12;
+                break;  
+            case "samsunggalaxya54":
+                dataSet[i].image = SamsungGalaxyA54;
+                break;
+            case "macbookair13":
+                dataSet[i].image = MacBookAir13;
+                break;   
+            case "asusrogstrix17":
+                dataSet[i].image = ASUSROGStrix17;
+                break; 
+            case "dellinspiron3511":
+                dataSet[i].image = DellInspiron3511;
+                break;         
+            case "airpods2":
+                dataSet[i].image = AirPods2;
+                break;  
+            case "sonywh1000xm4":
+                dataSet[i].image = SonyWH1000XM4;
+                break;  
+        }
+    }
+    return dataSet;
 }
 
 function Body(props) {
@@ -90,9 +139,9 @@ function Body(props) {
         );
     }
     return (
-        <>
+        <div className="product-container">
             {filteredData.map((product) => <Product image={product.image} name={product.name} price={product.price} id={product.id}/>)}
-        </>
+        </div>
     );
 }
 
